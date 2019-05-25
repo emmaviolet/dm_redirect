c17523e2b554486f2f68baf8f657634c488a50a5
Emma Makinson <emmavioletmakinson@gmail.com>
2019-05-25 13:16:05 +0100

    Adds previously unsaved changes

--- app/js/background.js
+++ app/js/background.js
@@ -4,7 +4,6 @@
  * Background file - listens for url changes to tabs
  * If the new url is one of the user's blocked sites, updates the tab to redirect to the user's redirect site
  */
-
 const Tab = require('./tab.js');
 
 chrome.tabs.onUpdated.addListener(() => {
--- test/specs/tab_spec.js
+++ test/specs/tab_spec.js
@@ -3,41 +3,41 @@
 const chrome = require('sinon-chrome');
 const Tab = require('../../app/js/tab.js');
 
-describe('tab', function () {
+describe('tab', () => {
     'use strict';
 
-    before(function () {
+    before(() => {
         global.chrome = chrome;
     });
 
-    beforeEach(function () {
+    beforeEach(() => {
         chrome.runtime.sendMessage.flush();
     });
 
-    describe('constructor', function () {
-        it('creates a new tab item', function () {
+    describe('constructor', () => {
+        it('creates a new tab item', () => {
             var tab = new Tab({id: 389, url: 'https://google.com'});
             assert(tab);
         });
     });
 
-    describe('redirectIfBlocked', function () {
+    describe('redirectIfBlocked', () => {
 
-        describe('if neither blocked sites nor redirect url has been set', function () {
-            beforeEach(function () {
+        describe('if neither blocked sites nor redirect url has been set', () => {
+            beforeEach(() => {
                 chrome.storage.local.get.yields({});
             });
 
-            describe('and tab url is the default blocked site dailymail.co.uk', function () {
-                it('redirects to the default redirect url theguardian.com', function () {
+            describe('and tab url is the default blocked site dailymail.co.uk', () => {
+                it('redirects to the default redirect url theguardian.com', () => {
                     var tab = new Tab({id: 400, url: 'https://dailymail.co.uk'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(400, {url: 'http://theguardian.com'}).calledOnce);
                 });
             });
 
-            describe('and tab url is not the default blocked site dailymail.co.uk', function () {
-                it('does not redirect', function () {
+            describe('and tab url is not the default blocked site dailymail.co.uk', () => {
+                it('does not redirect', () => {
                     var tab = new Tab({id: 444, url: 'https://google.com'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(444, {url: 'http://theguardian.com'}).notCalled);
@@ -45,21 +45,21 @@ describe('tab', function () {
             });
         });
 
-        describe('if blocked sites have been set but redirect url has not been set', function () {
-            beforeEach(function () {
+        describe('if blocked sites have been set but redirect url has not been set', () => {
+            beforeEach(() => {
                 chrome.storage.local.get.yields({blockedSites: ['facebook.com', 'website.co.uk']});
             });
 
-            describe('and tab url is one of the blocked sites set', function () {
-                it('redirects to the default redirect url theguardian.com', function () {
+            describe('and tab url is one of the blocked sites set', () => {
+                it('redirects to the default redirect url theguardian.com', () => {
                     var tab = new Tab({id: 200, url: 'https://website.co.uk'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(200, {url: 'http://theguardian.com'}).calledOnce);
                 });
             });
 
-            describe('and tab url is not among the blocked sites set', function () {
-                it('does not redirect', function () {
+            describe('and tab url is not among the blocked sites set', () => {
+                it('does not redirect', () => {
                     var tab = new Tab({id: 250, url: 'https://dailymail.co.uk'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(250, {url: 'http://theguardian.com'}).notCalled);
@@ -67,21 +67,21 @@ describe('tab', function () {
             });
         });
 
-        describe('if redirect url has been set but blocked sites have not been set', function () {
-            beforeEach(function () {
+        describe('if redirect url has been set but blocked sites have not been set', () => {
+            beforeEach(() => {
                 chrome.storage.local.get.yields({redirectUrl: 'ft.com'});
             });
 
-            describe('and tab url is the default blocked site dailymail.co.uk', function () {
-                it('redirects to the http version of the redirect url set', function () {
+            describe('and tab url is the default blocked site dailymail.co.uk', () => {
+                it('redirects to the http version of the redirect url set', () => {
                     var tab = new Tab({id: 199, url: 'https://dailymail.co.uk'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(199, {url: 'http://ft.com'}).calledOnce);
                 });
             });
 
-            describe('and tab url is not the default blocked site dailymail.co.uk', function () {
-                it('does not redirect', function () {
+            describe('and tab url is not the default blocked site dailymail.co.uk', () => {
+                it('does not redirect', () => {
                     var tab = new Tab({id: 197, url: 'https://facebook.com'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(197, {url: 'http://ft.com'}).notCalled);
@@ -89,21 +89,21 @@ describe('tab', function () {
             });
         });
 
-        describe('if both the redirect url and the blocked sites have been set', function () {
-            beforeEach(function () {
+        describe('if both the redirect url and the blocked sites have been set', () => {
+            beforeEach(() => {
                 chrome.storage.local.get.yields({blockedSites: ['facebook.com', 'google.com'], redirectUrl: 'ft.com'});
             });
 
-            describe('and tab url is one of the blocked sites set', function () {
-                it('redirects to the http version of the redirect url set', function () {
+            describe('and tab url is one of the blocked sites set', () => {
+                it('redirects to the http version of the redirect url set', () => {
                     var tab = new Tab({id: 180, url: 'https://facebook.com'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(180, {url: 'http://ft.com'}).calledOnce);
                 });
             });
 
-            describe('and tab url is not one of the blocked sites set', function () {
-                it('does not redirect', function () {
+            describe('and tab url is not one of the blocked sites set', () => {
+                it('does not redirect', () => {
                     var tab = new Tab({id: 185, url: 'https://pinterest.com'});
                     tab.redirectIfBlocked();
                     assert.ok(chrome.tabs.update.withArgs(185, {url: 'http://ft.com'}).notCalled);
@@ -112,7 +112,7 @@ describe('tab', function () {
         });
     });
 
-    after(function () {
+    after(() => {
         chrome.flush();
         delete global.chrome;
     });
