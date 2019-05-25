0896ba7012f4225b55e3bcc0eb15b2b4efed0dbc
Emma Makinson <emmavioletmakinson@gmail.com>
2019-05-25 09:46:50 +0100

    Switch to JSHint

--- app/js/background.js
+++ app/js/background.js
@@ -1,5 +1,11 @@
 /*global chrome */
 
+/*
+    Background
+    Listens for url changes
+    If the new url is one of the user's blocked sites, updates the tab to redirect to the user's redirect site
+*/
+
 const Tab = require('./tab.js');
 
 chrome.tabs.onUpdated.addListener(function () {
--- app/js/blocked_list.js
+++ app/js/blocked_list.js
@@ -2,27 +2,25 @@
 
 const Website = require('./website.js');
 
-function unblockSite(targetId) {
+/**
+ * Iterates through the given items and forms a list with the url name and an 'unblock' button for each one
+ */
+function listForItems(items) {
     'use strict';
 
-    var url = targetId.replace('unblock-button-', '');
-    var website = new Website(url);
-    website.unblock(function () {
-        window.location.href = '/app/views/status.html';
-    });
-}
-
-function constructHTMLforBlockedSites(sites) {
-    'use strict';
+    if (!Array.isArray(items) || !items.length) {
+        // This shouldn't happen so in the future we probably want to log an issue
+        return 'You have no blocked sites';
+    }
 
     var htmlString = '';
 
-    sites.forEach(function (item, index) {
+    items.forEach(function (item, index) {
         htmlString += '<div class="blocked-site row">' + item + '<a href="#" id="unblock-button-';
         htmlString += item;
         htmlString += '" class="unblock-link" style="float: right;">Unblock</a></div>';
 
-        if (index !== sites.length - 1) {
+        if (index !== items.length - 1) {
             htmlString += '<div class="border-line"></div>';
         }
     });
@@ -30,24 +28,24 @@ function constructHTMLforBlockedSites(sites) {
     return htmlString;
 }
 
+/**
+ * Constructs an html list of all the user's blocked sites and adds an event listener to each button,
+ * to unblock the corresponding site if the button is clicked.
+ */
 chrome.storage.local.get(['blockedSites'], function (items) {
     'use strict';
 
-    var blockedSites = items.blockedSites;
-    var html;
-
-    if (!Array.isArray(blockedSites) || !blockedSites.length) {
-        // This shouldn't happen so in the future we probably want to log an issue
-        html = 'You have no blocked sites';
-    } else {
-        html = constructHTMLforBlockedSites(blockedSites);
-    }
-
-    document.getElementById('blocked-sites-list-content').innerHTML = html;
+    document.getElementById('blocked-sites-list-content').innerHTML = listForItems(items.blockedSites);
 
     document.getElementById('blocked-sites-list-content').addEventListener('click', function (event) {
         if (event.target && event.target.matches('a')) {
-            unblockSite(event.target.id);
+
+            var url = event.target.id.replace('unblock-button-', '');
+            var website = new Website(url);
+            website.unblock(function () {
+                window.location.href = '/app/views/status.html';
+            });
+
         }
     });
 });
